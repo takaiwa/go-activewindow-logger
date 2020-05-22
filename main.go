62e8330ffb5893e1fcb4c66902b1eb3831b8f0a6
Takaiwa29 <devtakaiwa@gmail.com>
2020-05-22 22:55:07 +0900

    Changed to output the idle time to the log every 5 minutes

--- main.go
+++ main.go
@@ -11,12 +11,23 @@ import (
 	"unsafe"
 )
 
-const LOG_FORMAT = "20060102"
+const LogFormat = "20060102"
+
+//const IdleWaitingTime = 5 * 1000	// 秒
+const IdleWaitingTime = 5 * 60 * 1000 // 5min
 
 var (
 	mod                     = windows.NewLazyDLL("user32.dll")
 	procGetWindowText       = mod.NewProc("GetWindowTextW")
 	procGetWindowTextLength = mod.NewProc("GetWindowTextLengthW")
+	user32                  = syscall.MustLoadDLL("user32.dll")
+	kernel32                = syscall.MustLoadDLL("kernel32.dll")
+	getLastInputInfo        = user32.MustFindProc("GetLastInputInfo")
+	getTickCount            = kernel32.MustFindProc("GetTickCount")
+	lastInputInfo           struct {
+		cbSize uint32
+		dwTime uint32
+	}
 )
 
 type (
@@ -49,13 +60,7 @@ func getWindow(funcName string) uintptr {
 	return hwnd
 }
 
-func ticker() error {
-	t := time.NewTicker(1 * time.Second)
-
-	date := time.Now()
-	fileName := "./" + date.Format(LOG_FORMAT) + "_gawl.log"
-
-	// ログファイルの出力設定
+func getLogfile(fileName string) *os.File {
 	logfile, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		panic("cannot open:" + err.Error())
@@ -63,31 +68,65 @@ func ticker() error {
 	log.SetOutput(io.MultiWriter(logfile, os.Stdout))
 	log.SetFlags(log.Ldate | log.Ltime)
 
+	return logfile
+}
+
+func getLogFileName() string {
+	return "./" + time.Now().Format(LogFormat) + "_gawl.log"
+}
+
+func openLogfile(fileName *string, logfile *os.File) {
+	tmp := getLogFileName()
+	if *fileName != tmp {
+		// 日付が変わったら新しいログに書き込む
+		logfile.Close()
+		*fileName = tmp
+		logfile = getLogfile(tmp)
+	}
+}
+
+func getIdleTime() uint32 {
+	lastInputInfo.cbSize = uint32(unsafe.Sizeof(lastInputInfo))
+	currentTickCount, _, _ := getTickCount.Call()
+	r1, _, err := getLastInputInfo.Call(uintptr(unsafe.Pointer(&lastInputInfo)))
+	if r1 == 0 {
+		panic("error getting last input info: " + err.Error())
+	}
+	return uint32(currentTickCount) - lastInputInfo.dwTime
+}
+
+func ticker() error {
+	t := time.NewTicker(1 * time.Second)
+
+	fileName := getLogFileName()
+	logfile := getLogfile(fileName)
+
 	defer t.Stop()
 	defer logfile.Close()
 	prevText := ""
+	var counter uint32
 
 	for {
 		select {
 		case <-t.C:
-			if hwnd := getWindow("GetForegroundWindow"); hwnd != 0 {
-				text := GetWindowText(HWND(hwnd))
-				if prevText != text {
-					//fmt.Println("v:", v, "window :", text, "# hwnd:", hwnd)
-					tmp := "./" + time.Now().Format(LOG_FORMAT) + "_gawl.log"
-					if fileName != tmp {
-						// 日付が変わったら新しいログに書き込む
-						logfile.Close()
-						fileName = tmp
-						logfile, err = os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-						if err != nil {
-							panic("cannot open:" + err.Error())
-						}
-						log.SetOutput(io.MultiWriter(logfile, os.Stdout))
-						log.SetFlags(log.Ldate | log.Ltime)
+			idleTime := getIdleTime()
+			if idleTime < IdleWaitingTime {
+				counter = 1
+
+				if hwnd := getWindow("GetForegroundWindow"); hwnd != 0 {
+					text := GetWindowText(HWND(hwnd))
+					if prevText != text {
+						//fmt.Println("v:", v, "window :", text, "# hwnd:", hwnd)
+						openLogfile(&fileName, logfile)
+						log.Println(",", text)
+						prevText = text
 					}
-					log.Println(",", text)
-					prevText = text
+				}
+			} else {
+				if idleTime > (IdleWaitingTime * counter) {
+					openLogfile(&fileName, logfile)
+					log.Println(", Idle Time:", time.Duration(idleTime)*time.Millisecond)
+					counter = counter + 1
 				}
 			}
 		}
